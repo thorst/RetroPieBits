2a3818703b657513854151f51e451c9934257cfe
Todd Horst <toddmhorst@yahoo.com>
2016-08-19 19:00:47 -0400

    Update unzip.py

--- scripts/unzip.py
+++ scripts/unzip.py
@@ -1,5 +1,6 @@
 # May need to install 7zip
 # sudo apt-get install p7zip-full
+# sudo apt-get install unrar
 import glob
 import os.path
 import zipfile
@@ -7,7 +8,7 @@ import subprocess
 import sys
 
 scanDirs = ["/home/pi/RetroPie/roms/psx","/home/pi/RetroPie/roms/n64","/home/pi/RetroPie/roms/dreamcast","/home/pi/RetroPie/roms/snes"]
-types = ('*.zip', '*.7z') # the tuple of file types
+types = ('*.zip', '*.7z', '*.rar') # the tuple of file types
 
 subdir = True
 trues = ("Y", "Yes", "True", "T", True)
@@ -31,9 +32,9 @@ for dir in scanDirs:
         ext = os.path.splitext(fullFileName)[1]
         dir = os.path.dirname(file)
         os.chdir(dir)
-        
-        print("Done: " + fileName)
-
+		
+        print("Starting: " + fileName)
+		
         if ext == ".7z":
           command = ""
           if subdir:
@@ -50,7 +51,10 @@ for dir in scanDirs:
           else:
             zip_ref.extractall()
           zip_ref.close()
+		elif ext == ".rar":
+		  command = ""
+		  command = "unrar e file.rar"
 
-        os.remove(file)
+        #os.remove(file)
 
         print("Done: " + fileName)
